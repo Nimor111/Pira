524331e6cbc5aee524b8f105f4e7fcd9e802083f
Nimor111 <georgi.bojinov@hotmail.com>
2019-01-15 01:08:07 +0200

    [Frontend] Load reusable html with fetch

---
+++ views/js/html.js
@@ -0,0 +1,31 @@
+function fetchHead() {
+  fetch("partials/head.html")
+    .then(res => {
+      return res.text();
+    })
+    .then(html => {
+      document.getElementById("head").innerHTML = html;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function fetchNav() {
+  fetch("partials/nav.html")
+    .then(res => res.text())
+    .then(html => {
+      const div = document.createElement("div");
+      div.innerHTML = html;
+      document.body.appendChild(div);
+
+      const modalScript = document.createElement("script");
+      modalScript.src = "js/Modal.js";
+      const navScript = document.createElement("script");
+      navScript.src = "js/nav.js";
+      document.body.appendChild(modalScript);
+      document.body.appendChild(navScript);
+
+      checkLogin();
+    });
+}
